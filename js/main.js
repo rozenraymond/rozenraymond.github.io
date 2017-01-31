f7c74be49146335629438725c918a9ffb39b340d
zenrevilene <zen.revilene@gmail.com>
2017-01-31 22:42:11 +1100

    added two sections

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+     // Type JS for typing effect on the homepage
+     $(function(){
+          $(".text").typed({
+            strings: ["Rozen.", "Full Stack Web Developer.", "Front-End Developer.", "Back-End Developer.", "Foodie.", "Travel Enthusiast.", "Lover of Cultural Dance.", "Web Developer!"],
+            typeSpeed: 0.5
+          });
+      });
+
+     //  Skrollr for scrolling page animation initiation
+    //  skrollr.init();
+
+});
