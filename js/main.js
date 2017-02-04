0f5bddcc6df810f898ec52ff392260f70743c27f
zenrevilene <zen.revilene@gmail.com>
2017-02-04 22:17:19 +1100

    new layout

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
      // Type JS for typing effect on the homepage
      $(function(){
           $(".text").typed({
-            strings: ["Rozen.", "Full Stack Web Developer.", "Front-End Developer.", "Back-End Developer.", "Foodie.", "Travel Enthusiast.", "Lover of Cultural Dance.", "Web Developer!"],
-            typeSpeed: 0.5
+            strings: ["Rozen.", "a Full-Stack Web Developer.", "a Front-End Developer.", "a Back-End Developer.", "a Foodie.", "a Travel Enthusiast.", "a Lover of Cultural Dance.", "an awesome Web Developer! ;)"],
+            typeSpeed: 20
           });
       });
 
      //  Skrollr for scrolling page animation initiation
-    //  skrollr.init();
+     skrollr.init();
 
 });
