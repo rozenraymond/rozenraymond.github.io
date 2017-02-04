5221b55d5e95f3744c70bf17d50e8f66cf18d1d7
zenrevilene <zen.revilene@gmail.com>
2017-02-05 00:01:33 +1100

    updated js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,1 @@
-$(document).ready(function(){
-     // Type JS for typing effect on the homepage
-     $(function(){
-          $(".text").typed({
-            strings: ["Rozen.", "a Full-Stack Web Developer.", "a Front-End Developer.", "a Back-End Developer.", "a Foodie.", "a Travel Enthusiast.", "a Lover of Cultural Dance.", "an awesome Web Developer! ;)"],
-            typeSpeed: 20
-          });
-      });
 
-     //  Skrollr for scrolling page animation initiation
-     skrollr.init();
-
-});
