defaeca3516f2b70efb5d6c47acedb1178737f99
zenrevilene <zen.revilene@gmail.com>
2017-02-07 13:52:16 +1100

    updated project's link

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+$(document).ready(function() {
 
+    $(".email").hover(function () {
+        $(this).toggleClass("animated");
+        $(this).toggleClass("pulse");
+        $(this).toggleClass("infinite");
+     });
+
+     $(".linkedin").hover(function () {
+         $(this).toggleClass("animated");
+         $(this).toggleClass("pulse");
+         $(this).toggleClass("infinite");
+      });
+
+      $(".github").hover(function () {
+          $(this).toggleClass("animated");
+          $(this).toggleClass("pulse");
+          $(this).toggleClass("infinite");
+       });
+
+       $(".twitter").hover(function () {
+           $(this).toggleClass("animated");
+           $(this).toggleClass("pulse");
+           $(this).toggleClass("infinite");
+        });
+
+});
